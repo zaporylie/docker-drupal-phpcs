c7a9457fd1d5f1406bfc1333a276849cf5743f26
Jakub Piasecki <jakub@nymedia.no>
2015-07-24 16:27:33 +0200

    Add debug option and sshkey less restriction validation

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,13 @@ RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local
 COPY test.sh /tmp/test.sh
 RUN chmod +x /tmp/test.sh
 
+RUN mkdir /root/.ssh
+RUN echo "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
 ENV SHA1=HEAD \
  BRANCH=7.x \
+ FORMAT=full \
+ DEBUG=FALSE \
  CLONE_URL=https://github.com/drupal/drupal.git
 
 ENTRYPOINT ["/bin/bash"]
--- test.sh
+++ test.sh
@@ -7,7 +7,11 @@ handleSigTerm()
 
 oneTimeSetUp()
 {
-        trap "handleSigTerm" TERM
+  trap "handleSigTerm" TERM
+  if [ ${DEBUG} = TRUE ]; then
+    ls -la $(dirname ${SSH_AUTH_SOCK})
+    cat /root/.ssh/config
+  fi
 	git clone --depth 10 --branch ${BRANCH} ${CLONE_URL} /tmp/test
 	cd /tmp/test
 	git checkout ${SHA1}
@@ -17,8 +21,8 @@ oneTimeSetUp()
 testPHPCodeSniffer() {
 	git log --pretty=oneline
 	FILES=$(git diff --name-only HEAD~1 | tr "\\n" " ")
-	$HOME/.composer/vendor/bin/phpcs --standard=$HOME/.composer/vendor/drupal/coder/coder_sniffer/Drupal --extensions="php,module,inc,install,test,profile,theme,js,css,info,txt" $FILES
-        assertEquals "PHP Code Sniffer haven't found any errors" 0 $?
+	$HOME/.composer/vendor/bin/phpcs --standard=$HOME/.composer/vendor/drupal/coder/coder_sniffer/Drupal --extensions="php,module,inc,install,test,profile,theme,js,css,info,txt" --report=${FORMAT} $FILES
+  assertEquals 0 $?
 }
 
 . shunit2
