c2ecbb6214be7ed992152cd6cbbd75cf04a51050
support <support@server24.nymedia.no>
2015-11-20 16:25:54 +0100

    wip

--- test.sh
+++ test.sh
@@ -13,22 +13,47 @@ if [ -d "${GIT_CLONE_PATH}" ]; then
 	rm -rf ${GIT_CLONE_PATH}
 fi
 
-git clone --depth 20 --branch ${GIT_BRANCH} ${GIT_CLONE_URL} ${GIT_CLONE_PATH}
+git clone --depth ${GIT_DEPTH} --branch ${GIT_BRANCH} ${GIT_CLONE_URL} ${GIT_CLONE_PATH} > /dev/null 2>&1
 
 # Check if error.
 if [ $? -ne 0 ]; then
 	exit $?
 fi
 
-cd ${GIT_CLONE_PATH}
-git checkout ${GIT_AFTER}
-git reset ${GIT_AFTER}
+cd ${GIT_CLONE_PATH} > /dev/null 2>&1
+git checkout ${GIT_AFTER} > /dev/null 2>&1
+
+# Check if error.
+if [ $? -ne 0 ]; then
+        exit $?
+fi
+
+git reset ${GIT_AFTER} > /dev/null 2>&1
+
+# Check if error.
+if [ $? -ne 0 ]; then
+        exit $?
+fi
 
 if [ ${DEBUG} = TRUE ]; then
 	git log --pretty=oneline
 fi
 
+if [ ${DEBUG} = TRUE ]; then
+	echo "all"
+        git diff --diff-filter=ACMRTUXB --name-only ${GIT_BEFORE}
+	echo "only matching extensions: ${EXTENSIONS}"
+        git diff --diff-filter=ACMRTUXB --name-only ${GIT_BEFORE} | grep ${EXTENSIONS}
+	echo "only matching extensions ${EXTENSIONS} and include patternt ${INCLUDE}"
+        git diff --diff-filter=ACMRTUXB --name-only ${GIT_BEFORE} | grep ${EXTENSIONS} | grep ${INCLUDE}
+fi
+
+
 FILES=$(git diff --diff-filter=ACMRTUXB --name-only ${GIT_BEFORE} | grep ${EXTENSIONS} | grep ${INCLUDE} | tr "\\n" " ")
+
+if [ ${DEBUG} = TRUE ]; then
+        echo ${FILES}
+fi
 
 if [ -z "$FILES" ]; then
 	exit 1;
